8b1f347b9b667e38edf8a6abaa7e9fa75b0f33d5
Shih-Hsiao <hsiao78611@gmail.com>
2017-07-06 10:15:45 +0800

    fixed some problems from previous push...#2

--- ks/utils/scrollpage.py
+++ ks/utils/scrollpage.py
@@ -1,36 +1,47 @@
 import time
 from random import randint
 
-def _scroll_down(self, load_more_button_Xpath, count_scroll):
+def _scroll_down(driver, load_more_button_Xpath, count_scroll, count_ex):
     button_location = 6600
     each_scroll = 6634
     scroll_range = button_location+each_scroll*count_scroll
 
-    try:
-        self.driver.execute_script('window.scrollTo(0,'+str(scroll_range)+');') # document.body.scrollHeight);')
+    driver.execute_script('window.scrollTo(0,'+str(scroll_range)+');') # document.body.scrollHeight);')
+    if count_ex == 0:
         count_scroll = count_scroll + 1
-        load_more_button = self.driver.find_element_by_xpath(load_more_button_Xpath)
-        button_visiable = load_more_button.get_attribute('style')  # .is_displayed() may occur error
-        if button_visiable != 'display: none;':
+    else:
+        count_scroll = count_scroll
+        # let it scroll back to click button
+        driver.execute_script('window.scrollTo(0,' + str(scroll_range-100) + ');')
+    load_more_button = driver.find_element_by_xpath(load_more_button_Xpath)
+    button_visiable = load_more_button.get_attribute('style')  # .is_displayed() may occur error
+    if button_visiable != 'display: none;':
+        try:
             load_more_button.click()
-        # wait for loading more projects
-        time.sleep(randint(5, 10))
-    except Exception as e:
-        # driver.save_screenshot('screenshot.png') # for debugging
-        print e
+        except Exception as e:
+            # driver.save_screenshot('screenshot.png') # for debugging
+            count_ex = count_ex + 1
+            print e
+    # wait for loading more projects
+    time.sleep(randint(5, 10))
 
-    return count_scroll
+
+    return [count_scroll, count_ex, button_visiable]
 
 
 def scroll_down_explore(driver, total):
     load_more_button_Xpath = '//*[@id="projects"]/div/div[2]'
     button_visiable = ''
     count_scroll = 0
+    count_ex = 0
     temp_count_visible_project = 0
 
     while button_visiable != 'display: none;':
         # scroll down and update scrolling time
-        count_scroll = _scroll_down(load_more_button_Xpath, count_scroll)
+        result = _scroll_down(driver, load_more_button_Xpath, count_scroll, count_ex)
+        count_scroll = result[0]
+        count_ex = result[1]
+        button_visiable = result[2]
         # track the state
         count_visible_project = len(driver.find_elements_by_xpath('//*[@data-project_state="successful"]'))
         if count_visible_project != temp_count_visible_project:
