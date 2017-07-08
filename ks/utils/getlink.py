00e801a46c2f6437ed202b659c0db873629619af
Shih-Hsiao <hsiao78611@gmail.com>
2017-07-09 05:25:59 +0800

    modified the SQL part of the previous push.

--- ks/utils/getlink.py
+++ ks/utils/getlink.py
@@ -11,9 +11,9 @@ def __init__(self):
     self.conn = sqlite3.connect(directory + '/' + 'explore.db')
 
 def proj_links(self):
-    df = pd.read_sql_query('SELECT link FROM explore', self.conn)
+    df = pd.read_sql_query('SELECT link FROM explore GROUP BY link', self.conn)
     return list(df['link'])
 
 def crt_links(self):
-    df = pd.read_sql_query('SELECT c_link FROM explore', self.conn)
+    df = pd.read_sql_query('SELECT c_link FROM explore GROUP BY c_link', self.conn)
     return list(df['c_link'])
