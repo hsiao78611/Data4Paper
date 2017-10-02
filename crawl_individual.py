1c3c42350a73c6ce8576e7230c0f94b69278d60c
Shih-Hsiao <hsiao78611@gmail.com>
2017-10-02 16:39:22 +0800

    need re-crawl updates and faqs of 1022 projects.

--- crawl_individual.py
+++ crawl_individual.py
@@ -32,10 +32,10 @@ if not os.path.exists(directory):
 #         df.to_csv(directory + '/' + file_name, mode='a', index=False, header=False, encoding='utf-8-sig')
 
 # Create connections.
-conn_proj = sqlite3.connect(directory + '/' + 'proj.db', timeout=10.0, check_same_thread=False)
+# conn_proj = sqlite3.connect(directory + '/' + 'proj.db', timeout=10.0, check_same_thread=False)
 # conn_rew = sqlite3.connect(directory + '/' + 'rew.db', timeout=10.0, check_same_thread=False)
 conn_upd = sqlite3.connect(directory + '/' + 'upd.db', timeout=10.0, check_same_thread=False)
-# conn_faq = sqlite3.connect(directory + '/' + 'faq.db', timeout=10.0, check_same_thread=False)
+conn_faq = sqlite3.connect(directory + '/' + 'faq.db', timeout=10.0, check_same_thread=False)
 # conn_cmt = sqlite3.connect(directory + '/' + 'cmt.db', timeout=10.0, check_same_thread=False)
 # conn_time = sqlite3.connect(directory + '/' + 'time.db', timeout=10.0, check_same_thread=False)
 
